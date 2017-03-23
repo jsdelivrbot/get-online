21700c90e8ebf08eb52c5b4f6c3a23a6694731ee
Pratik K <pkarnawat@godaddy.com>
2017-03-22 23:15:49 -0700

    Activate react on client side for rendering

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-console.log('Hi from client');
+ReactDOM.render(<App />, document.getElementById('app'));
