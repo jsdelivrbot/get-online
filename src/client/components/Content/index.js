ead018a4ea61b78ad039b0f2bb26ecf66bb0d4b6
Pratik K <pkarnawat@godaddy.com>
2017-03-26 23:35:56 -0700

    Update webpack config, create generic components and add some styles

---
+++ src/client/components/Content/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div id='app-content' className=''>
+      <p>App content</p>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/client/components/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  return (
+    <div id='app-header' className=''>
+      <p>Pratik K</p>
+    </div>
+  );
+}
+
+export default Header;
