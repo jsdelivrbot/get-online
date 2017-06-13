a9b795291131b94bf97d7238f90ab55f463a38c8
Pratik K <pkarnawat@godaddy.com>
2017-06-12 20:11:55 -0700

    content updates

--- src/client/components/Content/About.js
+++ src/client/components/Content/About.js
@@ -12,19 +12,20 @@ const About = () => (
           />
           <h1 className='title-txt'> Pratik Karnawat </h1>
           <p> Software Engineer </p>
-          <p> Javascript / Node.js Enthusiast, UX / Web Design, Big Data </p>
+          <p> Javascript (ES6), Node.js, UI/UX, Scalable Web Applications, Flux Architecture, React </p>
+          <p> Data Structures and Algorithms, Big Data, Machine Learning </p>
         </div>
         <div className='col-xs-12'>
           <h1 className='title-txt'> Little bit about me </h1>
-          <p>
-            Hello, I am a Software Engineer working at GoDaddy where I help in developing the next generation of website builder - "GoCentral".
+          <p className='ta-justify'>
+            Hello, I am a Software Engineer working at GoDaddy where I help in developing the next generation internationally scaled website builder - "GoCentral".
             Since the early days of my programming career I have had the privilege of working as a full stack developer on numerous applications 
             and over these years I have learned many things the hard and the easy way about building software solutions and fortunately this learning process
             is still going on (which means I am not bored yet and I guess I will never be).
           </p>
-          <p>
-            Academically, I hold a Master's degree in Computer Science from Arizona State University, Tempe, AZ - USA and a bachelor's degree in Information
-            Technology from Shri Vaishnav Institute of Technology and Science, Indore, MP - India.
+          <p className='ta-justify'>
+            Academically, I hold a Master's degree in Computer Science from Arizona State University, Tempe, AZ - USA
+            and a bachelor's degree in Information Technology from Shri Vaishnav Institute of Technology and Science, Indore, MP - India.
           </p>
         </div>
       </div>
